4f25ab82b445bde95f7fdf183bf449527619eeba
Mvergilio <mvergiliodesouza@gmail.com>
2022-05-12 19:17:44 -0300

    add the jsut in time mode in the tailwing.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    node: "jit",
     content: ['./src/components/**/*.{js,jsx,tsx, ts}', './src/pages/**/*.{js,jsx,ts,tsx}', './public/**/*.html'], //add this line
     darkMode: false, // or 'media' or 'class'
     theme: {
