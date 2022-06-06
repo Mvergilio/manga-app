160b2a1da5d91d0ebccd8f33ad2312862a7bafea
Mvergilio <mvergiliodesouza@gmail.com>
2022-06-06 11:08:18 -0300

    add the sanity.js file

--- sanity.js
+++ sanity.js
@@ -1,1 +1,12 @@
 import { createClient, createCurrentUserHook, createImageUlrBuilder } from "next-sanity";
+
+export const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion: "2022-06-04",
+    useCdn: process.env.NODE_ENV === "production"
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUlrBuilder(config).image(source);
