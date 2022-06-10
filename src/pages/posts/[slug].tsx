b9a4c651d51d98bed53d9a25cbea4b245c7376bf
Mvergilio <mvergiliodesouza@gmail.com>
2022-06-10 14:01:49 -0300

    problem on function getstaticPaths

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,31 @@
-const Post = () => {
-    return <h1>page details</h1>
+import { sanityClient } from '../../../sanity'
+import { Post } from '../../../typing'
+
+const Post = ({ post }) => {
+    return <h1>{post.slug.current}</h1>
+}
+
+export async function getStaticPaths() {
+    const query = `*[_type == "post"]{
+        _id,
+        title,
+        author->{
+        name,
+        image
+      },
+      description,
+      mainImage,
+      slug,
+      body
+      }`
+    const posts: [Post] = await sanityClient.fetch(query)
+    const newPosts = posts.map(post => {
+        return {
+            params: { slug: post.slug.current },
+            fallback: false // false or 'blocking'
+        }
+    })
+    return newPosts
 }
 
 export default Post
